f54fe7971cd9c3867db93240016d1cac1927f3f3
Boris Poloshkov <bprnd2@yandex.ru>
2023-11-11 18:57:16 +0300

    finish pbt, format and lint, add workflow

--- src/TreeMap.hs
+++ src/TreeMap.hs
@@ -1,19 +1,18 @@
-module TreeMap
-    (Tree, put, get, size, removeKey, containsKey, mapFromList, filterMap, foldlMap, foldrMap, mapMap, mergeMaps) where
+module TreeMap (Tree (..), put, get, size, removeKey, containsKey, mapFromList, filterMap, foldlMap, foldrMap, mapMap, mergeMaps) where
 
-data Tree k v = Nil | Node { keyvalue :: (k, v), leftchild :: (Tree k v), rightchild :: (Tree k v) } deriving (Show, Eq)
+data Tree k v = Nil | Node {keyvalue :: (k, v), leftchild :: Tree k v, rightchild :: Tree k v} deriving (Show, Eq)
 
 --- PUT ---
 
 put :: (Ord k) => k -> v -> Tree k v -> Tree k v
 put k v Nil = Node (k, v) Nil Nil
-put k v (Node kv@(k', v') left right) 
-    | k == k' = Node (k, v) left right
-    | k < k' = Node kv (put k v left) right
-    | k > k' = Node kv left (put k v right)
+put k v (Node kv@(k', v') left right)
+  | k == k' = Node (k, v) left right
+  | k < k' = Node kv (put k v left) right
+  | k > k' = Node kv left (put k v right)
 
 putKV :: (Ord k) => (k, v) -> Tree k v -> Tree k v
-putKV (k, v) tree = put k v tree
+putKV (k, v) = put k v
 
 putIf :: (Ord k) => (k -> Bool) -> k -> v -> Tree k v -> Tree k v
 putIf pred k v tree = if pred k then put k v tree else tree
@@ -23,10 +22,9 @@ putIf pred k v tree = if pred k then put k v tree else tree
 get :: (Ord k) => k -> Tree k v -> Maybe v
 get k Nil = Nothing
 get k (Node (k', v') left right)
-    | k == k' = Just v'
-    | k < k' = get k left
-    | k > k' = get k right
-
+  | k == k' = Just v'
+  | k < k' = get k left
+  | k > k' = get k right
 
 --- SIZE ---
 
@@ -41,9 +39,9 @@ size (Node _ l r) = 1 + size l + size r
 removeKey :: (Ord k) => k -> Tree k v -> Tree k v
 removeKey _ Nil = Nil
 removeKey k n@(Node kv@(k', _) left right)
-    | k == k' = removeRoot n
-    | k < k' = Node kv (removeKey k left) right
-    | k > k' = Node kv left (removeKey k right)
+  | k == k' = removeRoot n
+  | k < k' = Node kv (removeKey k left) right
+  | k > k' = Node kv left (removeKey k right)
 
 -- In case node has no successors -> just remove
 -- In case node has one successor -> swap with the successor and remove
@@ -53,9 +51,10 @@ removeRoot Nil = Nil
 removeRoot (Node _ Nil Nil) = Nil
 removeRoot (Node _ left Nil) = left
 removeRoot (Node _ Nil right) = right
-removeRoot (Node kv left right) = Node (kv') (left) (right')
-    where kv' = keyvalue $ leftmost right
-          right' = removeLeftmost right
+removeRoot (Node kv left right) = Node kv' left right'
+  where
+    kv' = keyvalue $ leftmost right
+    right' = removeLeftmost right
 
 leftmost Nil = Nil
 leftmost n@(Node _ Nil _) = n
@@ -63,66 +62,62 @@ leftmost (Node _ left _) = leftmost left
 
 removeLeftmost Nil = Nil
 removeLeftmost (Node _ Nil right) = right
-removeLeftmost (Node kv left right) = Node kv (removeLeftmost left) right 
-
+removeLeftmost (Node kv left right) = Node kv (removeLeftmost left) right
 
 --- CONTAINS KEY ---
 
 containsKey :: (Ord k) => k -> Tree k v -> Bool
 containsKey k node = case get k node of
-    Nothing -> False
-    _ -> True
-
+  Nothing -> False
+  _ -> True
 
 --- MAKE MAP FROM LIST OF PAIRS ---
 
 mapFromList :: (Ord k) => [(k, v)] -> Tree k v
-mapFromList = foldr putPair Nil where
+mapFromList = foldr putPair Nil
+  where
     putPair (k, v) = put k v
-
 
 --- FILTER ---
 
 filterMap :: (Ord k) => (k -> Bool) -> Tree k v -> Tree k v
-filterMap predi tree = filterHelper predi tree Nil where
+filterMap predi tree = filterHelper predi tree Nil
+  where
     filterHelper _ Nil new = new
     filterHelper predi' (Node (k, v) left right) new =
-        let filterLeft = filterHelper predi' left (putIf predi' k v new)
-        in filterHelper predi' right filterLeft
+      let filterLeft = filterHelper predi' left (putIf predi' k v new)
+       in filterHelper predi' right filterLeft
 
 --- FOLDS ---
 
 foldlMap :: (Ord k) => (b -> (k, v) -> b) -> b -> Tree k v -> b
 foldlMap _ ini Nil = ini
-foldlMap f ini (Node kv left right) = 
-    let leftResult = foldlMap f (f ini kv) left
-    in foldlMap f leftResult right
+foldlMap f ini (Node kv left right) =
+  let leftResult = foldlMap f (f ini kv) left
+   in foldlMap f leftResult right
 
-
-foldrMap :: (Ord k) => ((k,v) -> b -> b) -> b -> Tree k v -> b
+foldrMap :: (Ord k) => ((k, v) -> b -> b) -> b -> Tree k v -> b
 foldrMap _ ini Nil = ini
-foldrMap f ini (Node kv left right) = 
-    let rightResult = kv `f` foldrMap f ini right
-    in foldrMap f rightResult left
-
+foldrMap f ini (Node kv left right) =
+  let rightResult = kv `f` foldrMap f ini right
+   in foldrMap f rightResult left
 
 --- Map ---
 
-mapMap :: (Ord k, Ord k0) => ((k, v) -> (k0, v0)) -> Tree k v -> Tree k0 v0 
+mapMap :: (Ord k, Ord k0) => ((k, v) -> (k0, v0)) -> Tree k v -> Tree k0 v0
 mapMap mapper node = helperMap mapper node Nil
 
 helperMap :: (Ord k, Ord k0) => ((k, v) -> (k0, v0)) -> Tree k v -> Tree k0 v0 -> Tree k0 v0
 helperMap _ Nil new = new
-helperMap mapper (Node kv left right) new = 
-    let leftResult = helperMap mapper left new
-    in helperMap mapper right (putKV (mapper kv) leftResult)
-
+helperMap mapper (Node kv left right) new =
+  let leftResult = helperMap mapper left new
+   in helperMap mapper right (putKV (mapper kv) leftResult)
 
 --- Merge ---
 
 mergeMaps :: (Ord k) => Tree k v -> Tree k v -> Tree k v
 mergeMaps Nil m2 = m2
 mergeMaps m1 Nil = m1
-mergeMaps n1 (Node kv2 left2 right2) = 
-    let leftResult = mergeMaps (putKV kv2 n1) left2
-    in mergeMaps leftResult right2
+mergeMaps n1 (Node kv2 left2 right2) =
+  let leftResult = mergeMaps (putKV kv2 n1) left2
+   in mergeMaps leftResult right2
--- test/Test.hs
+++ test/Test.hs
@@ -1,72 +1,125 @@
-import Test.HUnit(assertEqual, runTestTTAndExit, Test(..))
-
+import Data.List (sort)
+import Data.Set (fromList, toList)
+import Test.HUnit (Test (..), assertEqual, runTestTTAndExit)
+import Test.QuickCheck
 import TreeMap
-    (Tree, put, get, size, removeKey, containsKey, mapFromList, filterMap, foldlMap, foldrMap, mapMap, mergeMaps)
+  ( Tree (..),
+    containsKey,
+    filterMap,
+    foldlMap,
+    foldrMap,
+    get,
+    mapFromList,
+    mapMap,
+    mergeMaps,
+    put,
+    removeKey,
+    size,
+  )
 
 main :: IO ()
 main = do
-    runTestTTAndExit tests
+  quickCheck pbt_merge
+  quickCheck pbt_neutral
+  quickCheck pbt_filter
+  quickCheck pbt_fold
+  runTestTTAndExit tests
 
 tests :: Test
-tests = TestList [
-    TestLabel "test basic map operations" testBaseOperations,
-    TestLabel "test filter" testFilter,
-    TestLabel "test fold" testFold,
-    TestLabel "test map" testMap,
-    TestLabel "test merge" testMerge
+tests =
+  TestList
+    [ TestLabel "test basic map operations" testBaseOperations,
+      TestLabel "test filter" testFilter,
+      TestLabel "test fold" testFold,
+      TestLabel "test map" testMap,
+      TestLabel "test merge" testMerge
     ]
 
 testBaseOperations :: Test
-testBaseOperations = TestCase ( do
-    let treeMap = mapFromList [(4, "four"), (3, "thr33"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
-    assertEqual "Test assert size" 5 (TreeMap.size treeMap)
-    assertEqual "Test assert get 4" (Just "four") (TreeMap.get 4 treeMap)
-    assertEqual "Test assert get 3" (Just "thr33") (TreeMap.get 3 treeMap) 
-    assertEqual "Test assert contains 11" True (TreeMap.containsKey 11 treeMap) 
-    assertEqual "Test assert does not contain 8" False (TreeMap.containsKey 8 treeMap) 
+testBaseOperations =
+  TestCase
+    ( do
+        let treeMap = mapFromList [(4, "four"), (3, "thr33"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
+        assertEqual "Test assert size" 5 (TreeMap.size treeMap)
+        assertEqual "Test assert get 4" (Just "four") (TreeMap.get 4 treeMap)
+        assertEqual "Test assert get 3" (Just "thr33") (TreeMap.get 3 treeMap)
+        assertEqual "Test assert contains 11" True (TreeMap.containsKey 11 treeMap)
+        assertEqual "Test assert does not contain 8" False (TreeMap.containsKey 8 treeMap)
 
-    let afterPut = TreeMap.put 8 "eight" treeMap
-    assertEqual "Test assert size changed" 6 (TreeMap.size afterPut) 
-    assertEqual "Test assert new key is present" True (TreeMap.containsKey 8 afterPut) 
-    assertEqual "Test assert new key value is correct" (Just "eight") (TreeMap.get 8 afterPut) 
+        let afterPut = TreeMap.put 8 "eight" treeMap
+        assertEqual "Test assert size changed" 6 (TreeMap.size afterPut)
+        assertEqual "Test assert new key is present" True (TreeMap.containsKey 8 afterPut)
+        assertEqual "Test assert new key value is correct" (Just "eight") (TreeMap.get 8 afterPut)
 
-    let afterRemove = TreeMap.removeKey 11 afterPut
-    assertEqual "Test assert size changed" 5 (TreeMap.size afterRemove) 
-    assertEqual "Test assert removed key is absent" False (TreeMap.containsKey 11 afterRemove)
-    assertEqual "Test assert removed key is absent" Nothing (TreeMap.get 11 afterRemove) 
+        let afterRemove = TreeMap.removeKey 11 afterPut
+        assertEqual "Test assert size changed" 5 (TreeMap.size afterRemove)
+        assertEqual "Test assert removed key is absent" False (TreeMap.containsKey 11 afterRemove)
+        assertEqual "Test assert removed key is absent" Nothing (TreeMap.get 11 afterRemove)
     )
 
 testFilter :: Test
-testFilter = TestCase ( do
-    let treeMap = filterMap (> 9) $ mapFromList [(4, "four"), (3, "thr33"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
-    assertEqual "Test assert size" 2 (TreeMap.size treeMap)    
+testFilter =
+  TestCase
+    ( do
+        let treeMap = filterMap (> 9) $ mapFromList [(4, "four"), (3, "thr33"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
+        assertEqual "Test assert size" 2 (TreeMap.size treeMap)
     )
-
 
 testFold :: Test
-testFold = TestCase ( do
-    let tree = mapFromList [(4, "four"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
-    let sumKeysLeft = foldlMap (\cnt (k,_) -> cnt + k) 0 tree
-    assertEqual "Test assert sum keys left" 127 sumKeysLeft
-    let sumKeysRight = foldrMap ((+) . fst) 0 tree
-    assertEqual "Test assert sum keys" 127 sumKeysRight
+testFold =
+  TestCase
+    ( do
+        let tree = mapFromList [(4, "four"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
+        let sumKeysLeft = foldlMap (\cnt (k, _) -> cnt + k) 0 tree
+        assertEqual "Test assert sum keys left" 127 sumKeysLeft
+        let sumKeysRight = foldrMap ((+) . fst) 0 tree
+        assertEqual "Test assert sum keys" 127 sumKeysRight
     )
-
 
 testMap :: Test
-testMap = TestCase ( do
-    let treeBefore = mapFromList [(1 :: Integer, 2 :: Integer), (3 :: Integer, 4 :: Integer), (5 :: Integer, 6 :: Integer)]
-    let treeAfterExpected = mapFromList [(6, 12), (4, 8), (2, 4)]
-    let treeAfterActual = mapMap (\(k, v) -> (k + 1, v * 2)) treeBefore
-    assertEqual "Test assert size has not changed" (TreeMap.size treeBefore) (TreeMap.size treeAfterActual)
-    assertEqual "Test assert mapped tree equals expected" treeAfterExpected treeAfterActual
+testMap =
+  TestCase
+    ( do
+        let treeBefore = mapFromList [(1 :: Integer, 2 :: Integer), (3 :: Integer, 4 :: Integer), (5 :: Integer, 6 :: Integer)]
+        let treeAfterExpected = mapFromList [(6, 12), (4, 8), (2, 4)]
+        let treeAfterActual = mapMap (\(k, v) -> (k + 1, v * 2)) treeBefore
+        assertEqual "Test assert size has not changed" (TreeMap.size treeBefore) (TreeMap.size treeAfterActual)
+        assertEqual "Test assert mapped tree equals expected" treeAfterExpected treeAfterActual
     )
-
 
 testMerge :: Test
-testMerge = TestCase ( do
-    let firstTree = mapFromList [(4, "four"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
-    let secondTree = mapFromList[(11, "eleven"), (10, "ten"), (100, "hundred"), (88, "88")]
-    let merged = mergeMaps firstTree secondTree
-    assertEqual "Test assert size is correct" 7 (TreeMap.size merged)
+testMerge =
+  TestCase
+    ( do
+        let firstTree = mapFromList [(4, "four"), (11, "eleven"), (9, "nine"), (100, "hundred"), (3, "three")]
+        let secondTree = mapFromList [(11, "eleven"), (10, "ten"), (100, "hundred"), (88, "88")]
+        let merged = mergeMaps firstTree secondTree
+        assertEqual "Test assert size is correct" 7 (TreeMap.size merged)
     )
+
+-- verify that resulting map has all the keys from merged maps
+pbt_merge :: [(Int, Int)] -> [(Int, Int)] -> Bool
+pbt_merge pairs1 pairs2 = all containsKey' pairs1 && all containsKey' pairs2
+  where
+    containsKey' pair = containsKey (fst pair) (mergeMaps (mapFromList pairs1) (mapFromList pairs2))
+
+-- verify neutral element has no effect
+pbt_neutral :: [(Int, Int)] -> Bool
+pbt_neutral pairs = mapFromList pairs == mergeMaps (mapFromList pairs) TreeMap.Nil
+
+-- verify that filtered map still contains all the keys satisfying predicate
+-- and contains none keys not satisfying the predicate
+pbt_filter :: [(Int, Int)] -> Int -> Bool
+pbt_filter pairs minKey = do
+  let filteredMap = filterMap (> minKey) (mapFromList pairs)
+  let containsKey' x = containsKey x filteredMap
+  not $
+    any containsKey' (filter (<= minKey) (map fst pairs))
+      && all containsKey' (filter (> minKey) (map fst pairs))
+
+-- verify that right fold into list gives us sorted keyset
+pbt_fold :: [(Int, Int)] -> Bool
+pbt_fold pairs = do
+  let sortedNoDups = toList $ fromList (sort (map fst pairs))
+  let help (k, _) xs = k : xs
+  sortedNoDups == foldrMap help [] (mapFromList pairs)
